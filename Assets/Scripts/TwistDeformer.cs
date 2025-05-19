7536c08c51ff817b985e95587c94c3e5bbed06bb
Yang Luo <992534544@qq.com>
2025-05-19 14:04:58 +0800

    Add proto

--- Assets/Scripts/TwistDeformer.cs
+++ Assets/Scripts/TwistDeformer.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
-
 // [RequireComponent(typeof(MeshFilter))]
 public class TwistDeformer : MonoBehaviour
 {
---
+++ Assets/Scripts/ZmqClient.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using NetMQ;
+using NetMQ.Sockets;
+using Google.Protobuf;
+using Sea;
+
+public class ZmqClient : MonoBehaviour
+{
+
+    private Thread zmqThread;
+    
+    public bool isRunning = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    public void StartCommunication()
+    {
+        
+        Debug.Log("开启反馈线程");
+        isRunning = true;
+        
+        zmqThread = new Thread(() => {
+                AsyncIO.ForceDotNet.Force();
+                using (var subSocket = new SubscriberSocket())
+                {
+                    subSocket.Connect("tcp://localhost:6060");
+                    subSocket.Subscribe("status");
+
+                    while (isRunning)
+                    {
+                        try
+                        {
+                            byte[] msg = subSocket.ReceiveFrameBytes();
+                            var feedback = ControlFeedback.Parser.ParseFrom(msg);
+                            switch (feedback.FeedbackCase)
+                            {
+                                case ControlFeedback.FeedbackOneofCase.Status:
+                                    var status = feedback.Status;
+                                    Debug.Log(status.ToString());
+                                    break;
+                                case ControlFeedback.FeedbackOneofCase.Config:
+                                    var config = feedback.Config;
+                                    Debug.Log(config.ToString());
+                                    break;
+                                case ControlFeedback.FeedbackOneofCase.None:
+                                    Debug.LogWarning("Feedback类型错误！");
+                                    break;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.LogError("Receive Error: " +e.Message);
+                        }
+                    }
+                }
+            }
+        );
+        
+    }
+}
